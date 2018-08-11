f82c19c973c8bd697d703b24a4c0b1fe1e611a17
gsrinivas27 <gsrinivasulu27@gmail.com>
2018-08-11 19:49:55 +0530

    sbfc

---
+++ src/main/java/com/sri/sboot/crud/CrudOperationsApplication.java
@@ -0,0 +1,12 @@
+package com.sri.sboot.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudOperationsApplication.class, args);
+	}
+}
