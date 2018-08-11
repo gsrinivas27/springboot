426a6b271694e9191a32f3a941e00b5870d50a7e
gsrinivas27 <gsrinivasulu27@gmail.com>
2018-08-11 19:38:43 +0530

    main

---
+++ TryForIt/src/com/sri/main/MainClass.java
@@ -0,0 +1,10 @@
+package com.sri.main;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("TryForDifferent");
+	}
+
+}
